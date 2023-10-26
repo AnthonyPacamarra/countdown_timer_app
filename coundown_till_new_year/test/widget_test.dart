527f0255fbfff39fedc435dae79889858abff172
AnthonyPacamarra <anthonygwapopacamarra@gmail.com>
2023-10-27 01:56:51 +0800

    Project Done

--- coundown_till_new_year/test/widget_test.dart
+++ coundown_till_new_year/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:coundown_till_new_year/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(CountdownTimerDemo());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
